80fa6d649489fe0fccf2d88bd87fbdeae1d61f08
Madhav Murikipudi <40741574+MadhavMurik@users.noreply.github.com>
2024-04-21 05:19:21 -0400

    Update client.py

--- finalProj/flask_app/client.py
+++ finalProj/flask_app/client.py
@@ -65,8 +65,8 @@ class SpotifyClient(object):
 
         data = resp.json()
 
-if "error" in data:
-    raise ValueError(f'[ERROR]: Error retrieving results: \'{data["error"]["message"]}\' ')
+        if "error" in data:
+            raise ValueError(f'[ERROR]: Error retrieving results: \'{data["error"]["message"]}\' ')
 
 
         search_results_json = data["tracks"]["items"]
